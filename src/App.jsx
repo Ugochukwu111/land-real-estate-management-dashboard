ae4bbed873d652a0a0bac89a3efac559dbed57be
Pascal <pas111cal3@gmail.com>
2026-04-27 15:23:59 +0100

    refactor: implement scalable routing  by nesting dashboard's children

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import { Navigate } from "react-router-dom";
 
 /*
 =================
@@ -12,7 +12,7 @@ import SignIn from "./Pages/Forms/SignIn";
 import ForgotPassword from "./Pages/Forms/ForgotPassword";
 import ResetPassword from "./Pages/Forms/ResetPassword";
 import ConfirmOtp from "./Pages/Forms/ConfirmOtp";
-
+import { DashBoardLayout } from "./Components/Admin/DashBoardLayout";
 
 /*
 =================
@@ -21,20 +21,35 @@ ADMIN PAGES
  */
 import AdminSignIn from "./Pages/Admin/AdminSignIn";
 import AdminDashBoardPage from "./Pages/Admin/AdminDashBoardPage";
+import AdminAssociatePage from "./Pages/Admin/AdminAssociatePage";
+import AdminIntelligencePage from "./Pages/Admin/AdminIntelligencePage";
+import AdminSettingsPage from "./Pages/Admin/AdminSettingsPage";
 
+import AdminUploadListing from "./Pages/Admin/AdminUploadListing";
+import AdminOpenDeals from "./Pages/Admin/AdminOpenDeals";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index element={<AdminDashBoardPage />} />
-
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/admin-sign-in" element={<AdminSignIn />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/confirm-otp" element={<ConfirmOtp />} />
+        <Route path="/admin/sign-in" element={<AdminSignIn />} />
+
+        <Route path="/admin" element={<DashBoardLayout isAdmin={true} />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<AdminDashBoardPage />} />
+          <Route path="associate" element={<AdminAssociatePage />} />
+          <Route path="settings" element={<AdminSettingsPage />} />
+          <Route path="intelligence" element={<AdminIntelligencePage />}>
+            <Route index element={<Navigate to="upload-listing" replace />} />
+            <Route path="upload-listing" element={<AdminUploadListing />} />
+            <Route path="open-deals" element={<AdminOpenDeals />} />
+          </Route>
+        </Route>
       </Routes>
     </>
   );
